bd94911aa1684c7c1eb1fdf6f869c3110ddf606b
cmanzi00 <cmanzi25@amherst.edu>
2023-12-12 11:06:42 -0500

    Updated Shiny App

--- Shiny/shiny.R
+++ Shiny/shiny.R
@@ -6,9 +6,11 @@ library(plotly)
 #For TAB 1 BoxPlot
 
 data <- readRDS("SOAI_AFINN.rds") |>
-  select(YearsCode, Age, EdLevel, total_score)             
+  mutate(NumYearsCode = as.numeric(YearsCode)) |>
+  select(NumYearsCode, Age, EdLevel, total_score)
 
-all_cols_values <- names(data)
+all_cols_values <- c("NumYearsCode", "Age", "EdLevel")
+all_cols_names <- c("Number of Years Coding (Years)", "Age of Developer (Years)", "Education Level")
 
 ############
 #    ui    #
@@ -18,28 +20,24 @@ ui <- fluidPage(
   title="Survey Results",
   fluidRow(
         selectInput(inputId = "y_val"
-                    , label = "Choose a variable of interest:"
-                    , choices = all_cols_values
+                    , label = "Choose a variable:"
+                    , choices = all_cols_names
                     , selected = "Age"),
-    
-        plotlyOutput(outputId = "box")
+        plotlyOutput(outputId = "box", width = "100%", height = "700px")
   )
 )
-
 
 ############
 # server   #
 ############
 server <- function(input,output){
-  
-  # TAB 1: BOXPLOT
-  
   output$box <- renderPlotly({
-    ggplot(data, aes_string(y = "total_score", x = input$y_val)) +
+    ggplot(data, aes_string(y = "total_score", x = all_cols_values[input$y_val == all_cols_names])) +
       geom_boxplot() +
-      labs(y = "Response Score", x = input$y_val)
-  })
-  }
+      labs(y = "Response Score", x = input$y_val) +
+      theme(axis.text.x = element_text(angle = 45))
+  })}
+
 ####################
 # call to shinyApp #
 ####################
